a8e75c60d92eedcfcd61f1dc42fdf96f6a915572
nikosev <nikos.ev@hotmail.com>
2018-06-12 15:30:49 +0300

    Change schema and modules to be compatible with PostgrSQL

--- oidc-server/src/main/resources/db/migration/V3___scopes.sql
+++ oidc-server/src/main/resources/db/migration/V3___scopes.sql
@@ -1,15 +1,15 @@
 --
--- System scopes, see https://wiki.surfnet.nl/pages/viewpage.action?spaceKey=P3GFeI2015&title=OpenID%20Connect%20Implementatie
+-- System scopess
 --
-
 
 INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
   ('openid', 'log in using your identity', 'user', false, true, false, null),
-  ('profile', 'basic profile information', 'list-alt', false, true, false, null),
-  ('email', 'email address', 'envelope', false, true, false, null),
-  ('phone', 'telephone number', 'bell', false, true, false, null),
+  ('profile', 'read your basic profile info', 'list-alt', false, false, false, null),
+  ('email', 'read your email address', 'envelope', false, false, false, null),
   ('address', 'physical address', 'home', false, true, false, null),
+  ('phone', 'telephone number', 'bell', false, true, false, null),
+  ('offline_access', 'access your info while not being logged in', 'time', false, false, false, null),
   ('organization', 'organization information', 'home', false, true, false, null),
   ('entitlement', 'entitlement information', 'home', false, true, false, null),
-  ('userids', 'userids information', 'home', false, true, false, null),
-  ('offline_access', 'offline access', 'time', false, false, false, null);
+  ('refeds_edu','read your federated identity for research and education','book', false, false, false, null),
+  ('userids', 'userids information', 'home', false, true, false, null);
